3fdbc3d0c3d5aae5b6a24b970d0664c081e5000a
Dararaksmey <1220.phem.dararaksmey@gmail.com>
2024-03-07 14:59:25 +0700

    update project

--- JavaScript/JavaScript/05-Guess-My-Number/starter/script.js
+++ JavaScript/JavaScript/05-Guess-My-Number/starter/script.js
@@ -12,7 +12,7 @@ document.querySelector('.score').textContent = 20;
 console.log(document.querySelector('.guess').value);
 //Set value to the input element 
 document.querySelector('.guess').value = 23;
-*/
+
 //Create a number to compare 
 let Correct_Number =Math.trunc(Math.random()*20) + 1;
 //Create a score number 
@@ -101,7 +101,7 @@ document.querySelector(".check").addEventListener('click',function(){
 ///////////////////////////////////////
 // Coding Challenge #1
 
-/* 
+
 Implement a game rest functionality, so that the player can make a new guess! Here is how:
 
 1. Select the element with the 'again' class and attach a click event handler
@@ -111,3 +111,96 @@ Implement a game rest functionality, so that the player can make a new guess! He
 
 GOOD LUCK 😀
 */
+// Refactoring Out code : THE DRY Principle:
+//Create a number to compare 
+let Correct_Number =Math.trunc(Math.random()*20) + 1;
+//Create a score number 
+let score = 20;
+//Create a point to compare when the score hit to zero
+const lose_point = 1;
+//document.querySelector('.number').textContent = Correct_Number;
+//Create a highsore 
+let highsore = 0;
+//Create a function to display the message for each condition
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
+//Create a function to display the score 
+const displayScore = function(score){
+    document.querySelector('.score').textContent = score;
+}
+//Create a function to display the number 
+const displayNumber = function(number){
+    document.querySelector('.number').textContent = number;
+}
+//Handling Click Event.
+document.querySelector(".check").addEventListener('click',function(){
+    const guess = Number( document.querySelector('.guess').value);
+    console.log(guess ,typeof(guess));
+    // if there are no guess number show No Number!
+    if (!guess){
+       displayMessage("No Number!");
+    } //When the player guess ccorrect
+    else if (guess === Correct_Number){
+        displayMessage("Congratulation  ! Correct Number");
+        //SO the DOM manipulation, we can also change styles.
+        // Change background color to green when the user guess the correct Number;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        // change the width of number box 
+        document.querySelector('.number').style.width = '30rem';
+        // Display the correst number 
+       displayNumber(Correct_Number);
+        // Display the highscore 
+       if (score > highsore){
+           highsore = score;
+           document.querySelector('.highscore').textContent = highsore;
+       }
+
+    }//When the player guess incorrect 
+    else if(guess !== Correct_Number){
+        if (score > lose_point){
+            // use a turnery operator
+            // ? mean if
+            // : mean else
+            //document.querySelector('.message').textContent = guess > Correct_Number ? 'Too hight' : "Too low";
+            displayMessage(guess > Correct_Number ? 'Too hight' : "Too low");
+            // Guess worng number - 1 score;
+            score --; // => score = score - 1;
+            // Display to score 
+            displayScore(score);
+
+        }else{
+            displayMessage("You lose");
+            displayScore(0) ;
+        }
+
+    } 
+    // When user guess number Too hight 
+    // 1 Select the element with the 'again' class and attach a click event handler
+    document.querySelector('.again').addEventListener('click',function(){
+    // 2. In the handler function, restore initial values of the score and secretNumber variables
+    //Restore value of score 
+    score = 20;
+    //Restore value of correst_number
+    Correct_Number =Math.trunc(Math.random()*20) + 1;
+    // Restore highscore 
+    //highsore = 0;
+    // 3. Restore the initial conditions of the message, number, score and guess input field
+    // Restore the initail condition of message
+    displayMessage('Start guessing...')
+    // Restore the initail condition of number
+    displayNumber('?');
+    // Restore the initail condition of score
+    displayScore(score)
+    // Restore the initail condition of guess
+    document.querySelector('.guess').value ='';
+    // 4. Also restore the original background color (#222) and number width (15rem)
+    //Restore the background 
+    document.querySelector('body').style.backgroundColor ='#222';
+    //Restore the width of number 
+    document.querySelector('.number').style.width ='15rem';
+    //Restore value of Hightscore
+   //document.querySelector('.highscore').textContent = score;
+
+    })
+});
