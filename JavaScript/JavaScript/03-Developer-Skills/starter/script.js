e566e78aa6902565da32ad4c7fc36667820d0d68
Dararaksmey <1220.phem.dararaksmey@gmail.com>
2024-03-07 14:20:57 +0700

    Update game setcion

--- JavaScript/JavaScript/03-Developer-Skills/starter/script.js
+++ JavaScript/JavaScript/03-Developer-Skills/starter/script.js
@@ -7,7 +7,7 @@
 // calculate the temperature amplitue. Keep in mind that sometime there might be a sensor
 // error.
 
-const temperature = [3,-2,-6,-1,'error',9,13,17,15,14,9,5];
+/* const temperature = [3,-2,-6,-1,'error',9,13,17,15,14,9,5];
 
 // 1) Understanding the problem
 // - What is temp amplitude? Answer: difference between highest and lowest temp
@@ -23,7 +23,6 @@ const temperature = [3,-2,-6,-1,'error',9,13,17,15,14,9,5];
 const calcTempAmplitude = function(temp) {
     let max = temp[0];
     let min = temp[0];
-
     for(let i = 0 ; i < temp.length; i ++ ){
         const curTemp = temp[i];
         // - How to ignore errors?
@@ -77,3 +76,59 @@ const calcTempAmplitudeNew = function(temp1,temp2) {
 //calcTempAmplitude([3,7,4,23]);
 const amplitudeNew = calcTempAmplitudeNew([3,5,1],[9,0,5]);
 console.log(amplitudeNew);
+
+const meansureKelvin = function(){
+    const meansuerment = {
+        type: 'temp',
+        unit: 'celsius',
+        // C) FIX convert 
+        value: Number(prompt("Degres celsius:")),
+        
+    };
+    console.log(meansuerment)
+    //Show object in table:
+    console.table(meansuerment);
+    const kelvin = meansuerment.value + 273;
+    return kelvin;
+};
+// A) IDENTIFY
+console.log(meansureKelvin()); */
+
+///////////////////////////////
+// Coding Challenge #1 
+/*
+    Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+    Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+    Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+    Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+    TEST DATA 1: [17, 21, 23]
+    TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+// A) Understand
+/*
+    1) Print every elemet out of array and then each elemet have a string C 
+*/
+// B) Break down 
+/*
+    1. print elemet of array
+    2. add string with array element
+
+const printForecase = function(arr){
+    let str= '';
+    for (let i = 0 ; i <= arr.length; i++){
+       const element = arr[i];
+       str += `${element}ºC in ${i + 1} days ... `;
+    }
+    console.log("...."+ str);
+}
+
+//Test data 1 
+let data = [17,21,23];
+//Test date 2 
+let data2 = [12, 5, -5, 0, 4];
+printForecase(data2);*/
--- JavaScript/JavaScript/05-Guess-My-Number/starter/script.js
+++ JavaScript/JavaScript/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,113 @@
 'use strict';
+/*
+//Conncettion front-end called DOM Manipulatin
+console.log(document.querySelector('.message').textContent);
+//Select Element . class message in HTML 
+document.querySelector('.message').textContent = 'Correct Number ! '
+//Select Element . class number in HTML 
+document.querySelector('.number').textContent = 13;
+//Select Element . class score in HTML 
+document.querySelector('.score').textContent = 20;
+//Using value property 
+console.log(document.querySelector('.guess').value);
+//Set value to the input element 
+document.querySelector('.guess').value = 23;
+*/
+//Create a number to compare 
+let Correct_Number =Math.trunc(Math.random()*20) + 1;
+//Create a score number 
+let score = 20;
+//Create a point to compare when the score hit to zero
+const lose_point = 1;
+//document.querySelector('.number').textContent = Correct_Number;
+//Create a highsore 
+let highsore = 0;
+//Handling Click Event.
+document.querySelector(".check").addEventListener('click',function(){
+    const guess = Number( document.querySelector('.guess').value);
+    console.log(guess ,typeof(guess));
+    // if there are no guess number show No Number!
+    if (!guess){
+        document.querySelector('.message').textContent ='No Number!'
+    } //When the player guess ccorrect
+    else if (guess === Correct_Number){
+        document.querySelector('.message').textContent ='Congratulation  ! Correct Number';
+        //SO the DOM manipulation, we can also change styles.
+        // Change background color to green when the user guess the correct Number;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        // change the width of number box 
+        document.querySelector('.number').style.width = '30rem';
+        // Display the correst number 
+        document.querySelector('.number').textContent = Correct_Number;
+        // Display the highscore 
+       if (score > highsore){
+           highsore = score;
+           document.querySelector('.highscore').textContent = highsore;
+       }
+
+    } // When user guess number Too hight 
+    else if (guess > Correct_Number){
+        if (score > lose_point){
+            document.querySelector('.message').textContent ='Too hight';
+            // Guess worng number - 1 score;
+            score --; // => score = score - 1;
+            // Display to score 
+            document.querySelector('.score').textContent = score;
+
+        }else{
+            document.querySelector('.message').textContent = 'You lost';
+            document.querySelector('.score').textContent = 0;
+        }
+    } // When user guess number Too low
+    else if (guess < Correct_Number){
+        if (score > lose_point){
+            document.querySelector('.message').textContent ='Too low';
+            score --; // => score = score - 1;
+            // Display to score 
+            document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = 'You lost';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+    // 1 Select the element with the 'again' class and attach a click event handler
+    document.querySelector('.again').addEventListener('click',function(){
+    // 2. In the handler function, restore initial values of the score and secretNumber variables
+    //Restore value of score 
+    score = 20;
+    //Restore value of correst_number
+    Correct_Number =Math.trunc(Math.random()*20) + 1;
+    // Restore highscore 
+    //highsore = 0;
+    // 3. Restore the initial conditions of the message, number, score and guess input field
+    // Restore the initail condition of message
+    document.querySelector('.message').textContent='Start guessing...'
+    // Restore the initail condition of number
+    document.querySelector('.number').textContent = '?';
+    // Restore the initail condition of score
+    document.querySelector('.score').textContent = score;
+    // Restore the initail condition of guess
+    document.querySelector('.guess').value ='';
+    // 4. Also restore the original background color (#222) and number width (15rem)
+    //Restore the background 
+    document.querySelector('body').style.backgroundColor ='#222';
+    //Restore the width of number 
+    document.querySelector('.number').style.width ='15rem';
+    //Restore value of Hightscore
+   //document.querySelector('.highscore').textContent = score;
+
+    })
+});
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
